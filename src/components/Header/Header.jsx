a128b6350462a0b894a904a705ac755e28a9cdbd
kimtechnos <franciskimani559@gmail.com>
2024-07-02 15:30:07 +0300

    implmenting the formik validation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,75 @@
+// import { Link } from "react-router-dom";
+// import React, { useState } from "react";z
+// import "./header.css";
+
+// const Header = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleLinkClick = () => {
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <div className="Navbar">
+//       <span className="nav-logo">Z.a.p.h Tours</span>
+//       <div className={`nav-items ${isOpen ? "open" : ""}`}>
+//         <Link to="/" onClick={handleLinkClick}>
+//           Home
+//         </Link>
+//         <Link to="/destination" onClick={handleLinkClick}>
+//           Destination
+//         </Link>
+//         <Link to="/trips" onClick={handleLinkClick}>
+//           Trip types
+//         </Link>
+//         <Link to="/contact" onClick={handleLinkClick}>
+//           Contact
+//         </Link>
+//       </div>
+//       <div
+//         className={`nav-toggle ${isOpen ? "open" : ""}`}
+//         onClick={() => setIsOpen(!isOpen)}
+//       >
+//         <div className="bar"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
 import "./header.css";
 
+const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
+
 const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = headerRef.current;
+      const sticky = window.scrollY > 0;
+      if (sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handleLinkClick = () => {
@@ -10,7 +77,7 @@ const Header = () => {
   };
 
   return (
-    <div className="Navbar">
+    <HeaderContainer ref={headerRef} className="Navbar">
       <span className="nav-logo">Z.a.p.h Tours</span>
       <div className={`nav-items ${isOpen ? "open" : ""}`}>
         <Link to="/" onClick={handleLinkClick}>
@@ -20,7 +87,7 @@ const Header = () => {
           Destination
         </Link>
         <Link to="/trips" onClick={handleLinkClick}>
-          Trip types
+          Trip Types
         </Link>
         <Link to="/contact" onClick={handleLinkClick}>
           Contact
@@ -32,7 +99,7 @@ const Header = () => {
       >
         <div className="bar"></div>
       </div>
-    </div>
+    </HeaderContainer>
   );
 };
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,34 +1,113 @@
+import React from "react";
+import { useFormik } from "formik";
 import Banner from "../../components/Header/Banner/Banner";
 import "./contact.css";
-import React from "react";
+import * as yup from "yup";
 
-const Contact = () => {
+function Contact() {
+  const validationSchema = yup.object({
+    fullName: yup
+      .string()
+      .required("Full name is required")
+      .min(4, "Should have a minimum of 4 characters")
+      .max(10, "Should have a maximum of 10 characters"),
+    email: yup
+      .string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    number: yup
+      .string()
+      .required("Phone number is required")
+      .matches(/^[0-9]+$/, "Must be only digits")
+      .min(10, "Must be exactly 10 digits")
+      .max(10, "Must be exactly 10 digits"),
+    message: yup.string().required("Message is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      number: "",
+      message: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form data", values);
+    },
+    validationSchema: validationSchema,
+  });
+
   return (
     <>
       <Banner
         maintitle="Unforgettable Adventures Begin Here: Contact Zaph Tours"
-        description=" Don't miss out! Secure your dream adventure with Zaph Tours"
+        description="Don't miss out! Secure your dream adventure with Zaph Tours"
       />
       <section className="contact-section">
         <div className="contact-section-form">
-          <form className="contact-form">
+          <form className="contact-form" onSubmit={formik.handleSubmit}>
             <div className="contact-form-group">
-              <label htmlFor="fullname">Full Name:</label>
-              <input type="text" id="fullname" className="contact-form-input" />
+              <label htmlFor="fullName">Full Name:</label>
+              <input
+                type="text"
+                id="fullName"
+                className="contact-form-input"
+                name="fullName"
+                value={formik.values.fullName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.fullName && formik.errors.fullName ? (
+                <p className="error">{formik.errors.fullName}</p>
+              ) : null}
             </div>
             <div className="contact-form-group">
               <label htmlFor="email">Email Address:</label>
-              <input type="email" id="email" className="contact-form-input" />
+              <input
+                type="email"
+                id="email"
+                className="contact-form-input"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <p className="error">{formik.errors.email}</p>
+              ) : null}
             </div>
             <div className="contact-form-group">
-              <label htmlFor="phone">Phone Number:</label>
-              <input type="number" id="phone" className="contact-form-input" />
+              <label htmlFor="number">Phone Number:</label>
+              <input
+                type="tel"
+                id="number"
+                className="contact-form-input"
+                name="number"
+                value={formik.values.number}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.number && formik.errors.number ? (
+                <p className="error">{formik.errors.number}</p>
+              ) : null}
             </div>
             <div className="contact-form-group">
               <label htmlFor="message">Message:</label>
-              <textarea id="message" className="contact-form-input"></textarea>
+              <textarea
+                id="message"
+                className="contact-form-input"
+                name="message"
+                value={formik.values.message}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              ></textarea>
+              {formik.touched.message && formik.errors.message ? (
+                <p className="error">{formik.errors.message}</p>
+              ) : null}
             </div>
-            <button className="submit-btn">Submit</button>
+            <button type="submit" className="submit-btn">
+              Submit
+            </button>
           </form>
         </div>
         <div className="contact-section-map">
@@ -42,6 +121,6 @@ const Contact = () => {
       </section>
     </>
   );
-};
+}
 
 export default Contact;
